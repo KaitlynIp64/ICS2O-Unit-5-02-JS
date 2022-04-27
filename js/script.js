1e5e88a4cfb5095bd329cff232a0938bcb9d7652
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-04-27 12:50:32 -0400

    added code

--- js/script.js
+++ js/script.js
@@ -4,6 +4,27 @@
 // Created on: Mar 2022
 // This file contains xxx
 
+"use strict";
+
 /**
- * This function xxx.
+ * Check servie worker.
  */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-5-02-JS/sw.js", {
+    scope: "/ICS2O-Unit-5-02-JS/",
+  });
+}
+
+/**
+ * This function determines if an integer is negative or positive.
+ */
+function myButtonClicked() {
+  const userInteger = parseInt(document.getElementById("integer").value);
+  if (userInteger > -1) {
+    document.getElementById("answer").innerHTML =
+      "The integer you input is positive.";
+  } else {
+    document.getElementById("answer").innerHTML =
+      "The integer you input is a negative.";
+  }
+}
